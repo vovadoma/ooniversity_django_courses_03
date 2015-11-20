6de89ad58b413dcef9ee9e0be6bc776da192fba4
vladimir <x500@ukr.net>
2015-11-20 13:17:43 +0200

    #3-1

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse('hello')
 
 urlpatterns = patterns('',
     url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', index, name='index'),
 )
