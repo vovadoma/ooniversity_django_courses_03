1a44ca770b51ae8c1e58017784cb422cae0933e6
vladimir <x500@ukr.net>
2015-11-21 11:07:51 +0200

    #3-1

--- pybursa/settings.py
+++ pybursa/settings.py
@@ -1,13 +1,3 @@
-"""
-Django settings for pybursa project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.7/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.7/ref/settings/
-"""
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
@@ -83,3 +73,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
+
--- pybursa/urls.py
+++ pybursa/urls.py
@@ -1,20 +1,12 @@
 from django.conf.urls import patterns, include, url
-from django import template
 from django.contrib import admin
-from views import contact
-from views import student_list
-from views import student_detail
-from views import index
-from . import views
-from django.conf.urls.static import static
+from django.http import HttpResponse
 
+def index(request):
+    return HttpResponse('hello')
 
 urlpatterns = patterns('',
-    #url(r'^polls/', include('polls.urls', namespace="polls")),
+    url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', index, name='index'),
-    url(r'^contact/', contact, name='contact'),
-    url(r'^student_list/', student_list, name='student_list'),
-    url(r'^student_detail/', student_detail, name='student_detail'),
-    
 )
