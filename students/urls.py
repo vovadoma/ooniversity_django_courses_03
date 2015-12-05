33a6bf7433e8634a58f655024980b1286899336c
vladimir <x500@ukr.net>
2015-12-05 19:34:03 +0200

    #73

--- students/urls.py
+++ students/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 from students import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.list_view, name='list'),
+    url(r'^$', views.list_view, name='list_view'),
     url(r'^(?P<student_id>\d+)/$', views.detail, name='detail'),
     url(r'^add/$', views.create, name='add'),
     url(r'^edit/(?P<student_id>\d+)/$', views.edit, name='edit'),
